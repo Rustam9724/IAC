b8c1dd9dd6d3be386779ce8b621ed030ad256d4e
rustam_9724 <rustam_9724@mail.ru>
2023-06-21 11:43:50 +0500

    init: sertificates section adaptive

---
+++ src/components/Sertificates.tsx
@@ -0,0 +1,37 @@
+function Sertificates() {
+    return (
+        <section className="page__sertificates sertificates">
+            <div className="sertificates__container _container">
+                <h2 className="sertificates__title">Наше оборудование прошло все возможные вариации контроля качества</h2>
+                <div className="sertificates__body">
+                    <div className="sertificates__sertificate sertificate">
+                        <div className="sertificate__image">
+                            <img src="./assets/sertificates/sertificate.png" alt="sertificate"/>
+                        </div>
+                        <p>Сертификат соответсвия</p>
+                    </div>
+                    <div className="sertificates__sertificate">
+                        <div className="sertificate__image">
+                            <img src="./assets/sertificates/sertificate.png" alt="sertificate"/>
+                        </div>
+                        <p>Сертификат соответсвия</p>
+                    </div>
+                    <div className="sertificates__sertificate">
+                        <div className="sertificate__image">
+                            <img src="./assets/sertificates/sertificate.png" alt="sertificate"/>
+                        </div>
+                        <p>Сертификат соответсвия</p>
+                    </div>
+                    <div className="sertificates__sertificate">
+                        <div className="sertificate__image">
+                            <img src="./assets/sertificates/sertificate.png" alt="sertificate"/>
+                        </div>
+                        <p>Сертификат соответсвия</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Sertificates;
