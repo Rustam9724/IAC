5a34ba894fd66f5ba857f3fe444a24bc62496e4c
rustam_9724 <rustam_9724@mail.ru>
2023-06-21 11:08:20 +0500

    init: sertificates section

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,13 @@
 import WellcomeSection from "./WellcomeSection";
 import Range from "./Range";
+import Sertificates from "./Sertificates";
 
 function Main() {
     return (
         <div className="page">
             <WellcomeSection />
             <Range />
+            <Sertificates />
         </div>
     )
 }
