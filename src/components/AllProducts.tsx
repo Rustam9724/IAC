f49334067b9934ff1ba9652bba2c7204c699cd32
rustam_9724 <rustam_9724@mail.ru>
2023-07-04 14:14:32 +0500

    init: open and close categories in all products page

--- src/components/AllProducts.tsx
+++ src/components/AllProducts.tsx
@@ -1,17 +1,28 @@
+import { useState } from 'react';
+
 function AllProducts() {
+    const [isFirstLevelOpen, setIsFirstLevelOpen] = useState(false);
+    const [isSecondLevelOpen, setIsSecondLevelOpen] = useState(false);
+    const [isThirdLevelOpen, setIsThirdLevelOpen] = useState(false);
+
+    const fisrtLevelClickHandler = () => {
+        setIsFirstLevelOpen(!isFirstLevelOpen);
+        setIsSecondLevelOpen(!isSecondLevelOpen);
+    }
+
     return (
         <div className="all-products">
             <div className="all-products__container _container">
                 <div className="all-products__categories categories">
-                    <div className="categories__first-level">
+                    <div className={`categories__first-level ${isFirstLevelOpen ? 'open' : null}`} onClick={fisrtLevelClickHandler}>
                         <p>Выбрать категорию</p>
                         <div></div>
                     </div>
-                    <div className="categories__second-level">
+                    <div className={`categories__second-level ${isSecondLevelOpen ? 'open' : null}`} onClick={() => setIsThirdLevelOpen(!isThirdLevelOpen)}>
                         <p>Технологическе линии</p>
-                        <div></div>
+                        <div className={`${isThirdLevelOpen ? 'open' : null}`}></div>
                     </div>
-                    <div className="categories__third-level">
+                    <div className={`categories__third-level ${isThirdLevelOpen ? 'open' : null}`}>
                         <p>Сушка овощей и фруктов</p>
                         <p>Консервирование грибов</p>
                         <p>Сушка абрикосов</p>
